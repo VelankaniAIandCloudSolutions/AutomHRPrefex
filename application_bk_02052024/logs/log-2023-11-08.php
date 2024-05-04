d199e47159df5af55ab84dffaa486f7d3a8523df
Ankit Agrawal <Ankit Agrawal@VTPPL.COM>
2024-05-04 16:03:09 +0530

    Main branch code updated

--- application_bk_02052024/logs/log-2023-11-08.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2023-11-08 12:38:44 --> Severity: error --> Exception: syntax error, unexpected token ";", expecting ")" /var/www/html/crm/application/libraries/App_module_installer.php 28
